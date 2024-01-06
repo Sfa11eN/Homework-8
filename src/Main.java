cb8a5f9dcc86a69f966d87df9ef2dcf944ab984f
Sfa11eN <sfallenn@gmail.com>
2024-01-06 14:59:43 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Массивы урок 1");
+
+        System.out.println("Задание 1");
+
+        int[] array = new int[]{1, 2, 3};
+        double[] newArray = {1.57, 7.654, 9.986};
+        int[] finalArray = new int[3];
+        finalArray[0] = 7;
+        finalArray[1] = 9;
+        finalArray[2] = 23;
+
+        System.out.println("Задание 2");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + (i == array.length - 1 ? System.lineSeparator() : ", "));
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + (i == newArray.length - 1 ? System.lineSeparator() : ", "));
+
+        }
+        for (int i = 0; i < finalArray.length; i++) {
+            System.out.print(finalArray[i] + (i == finalArray.length - 1 ? System.lineSeparator() : ", "));
+
+        }
+        System.out.println("Задание 3");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + (i == array.length + 1 ? System.lineSeparator() : ", "));
+
+        }
+        for (int i = newArray.length - 1; i >= 0; i--) {
+            System.out.print(newArray[i] + (i == newArray.length + 1 ? System.lineSeparator() : ", "));
+
+        }
+        for (int i = finalArray.length - 1; i >= 0; i--) {
+            System.out.print(finalArray[i] + (i== finalArray.length+1 ? System.lineSeparator() : ", "));
+
+        }
+    }
+
+}
