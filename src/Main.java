4ae67c42455a4a85346f894279380bd1f141b4ab
Sfa11eN <sfallenn@gmail.com>
2024-01-07 13:58:40 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -26,17 +28,28 @@ public class Main {
         }
         System.out.println("Задание 3");
         for (int i = array.length - 1; i >= 0; i--) {
-            System.out.print(array[i] + (i == array.length + 1 ? System.lineSeparator() : ", "));
+            System.out.print(array[i] + (i ==0  ? System.lineSeparator() : ", "));
 
         }
         for (int i = newArray.length - 1; i >= 0; i--) {
-            System.out.print(newArray[i] + (i == newArray.length + 1 ? System.lineSeparator() : ", "));
+            System.out.print(newArray[i] + (i ==0  ? System.lineSeparator() : ", "));
 
         }
         for (int i = finalArray.length - 1; i >= 0; i--) {
-            System.out.print(finalArray[i] + (i== finalArray.length+1 ? System.lineSeparator() : ", "));
+            System.out.print(finalArray[i] + (i ==0  ? System.lineSeparator() : ", "));
+        }
+
+        System.out.println("Задание 4");
+
+
+        for (int i =0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i]++;
+            }
 
         }
+        System.out.println(Arrays.toString(array));
+
     }
 
 }
